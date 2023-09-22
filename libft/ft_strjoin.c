5a36a052ff92a06864be0545b311631865e2d927
Badr Kaztaoui <bkaztaou@c3r5p3.1337.ma>
2023-09-22 18:15:46 +0100

    Add libft

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkaztaou <bkaztaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 01:01:56 by bkaztaou          #+#    #+#             */
+/*   Updated: 2022/11/05 01:10:19 by bkaztaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = ft_calloc((s1_len + s2_len + 1), sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_memmove(str, s1, s1_len);
+	ft_memmove(str + s1_len, s2, s2_len);
+	*(str + (s1_len + s2_len)) = '\0';
+	return (str);
+}
