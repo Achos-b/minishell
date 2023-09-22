fd1295c693bc8dac25d2ec7a8da1d8b09fae34f6
Badr Kaztaoui <bkaztaou@c3r5p3.1337.ma>
2023-09-22 17:52:28 +0100

    Init minishell repo

---
+++ include/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkaztaou <bkaztaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 17:28:43 by bkaztaou          #+#    #+#             */
+/*   Updated: 2023/09/22 17:30:57 by bkaztaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/* INCLUDES */
+# include <unistd.h>
+# include <signal.h>
+# include <stdio.h>
+
+#endif
